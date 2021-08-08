3b02495c4ee0efbaf3cdc77761c7726df799f7af
FrancoPagnotta <francoapagnotta@gmail.com>
2021-08-07 22:55:51 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+// PROJECTS
+
+// import hamburger menu
+import hamburgerMenu from "../dom/hamburger_menu.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e)=> {
+	hamburgerMenu(".panel-btn",".panel",".menu a");
+});
+// import hamburger menu end
+
